508a04026f702c4cfb06a30a1e8712a390d40ff9
MemeLady <alex.kolpakova2001@gmail.com>
2023-03-06 20:07:26 +0700

    Третий пункт лабораторной работы
    Добавлены переходы между интро

--- app/src/main/java/com/example/wordsfactory/activity_intro1.kt
+++ app/src/main/java/com/example/wordsfactory/activity_intro1.kt
@@ -1,11 +1,28 @@
 package com.example.wordsfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.viewpager.widget.ViewPager
 
 class activity_intro1 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro1)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@activity_intro1, FirstActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
+
+
     }
 }
