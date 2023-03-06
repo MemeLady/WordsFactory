11d41ded0e90d9188de1243b3f73788236a2453f
MemeLady <alex.kolpakova2001@gmail.com>
2023-03-06 11:40:01 +0700

    Третий пункт лабораторной работы
    Переход на экран Intro1

---
+++ app/src/main/java/com/example/wordsfactory/activity_intro1.kt
@@ -0,0 +1,11 @@
+package com.example.wordsfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_intro1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro1)
+    }
+}
