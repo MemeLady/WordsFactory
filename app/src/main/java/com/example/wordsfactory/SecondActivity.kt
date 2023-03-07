a4cc13296050a5ad77e164cec32b93776e024685
MemeLady <alex.kolpakova2001@gmail.com>
2023-03-07 15:08:59 +0700

    Функционал кнопки
    Стрелка влево переходит на интро

--- app/src/main/java/com/example/wordsfactory/SecondActivity.kt
+++ app/src/main/java/com/example/wordsfactory/SecondActivity.kt
@@ -1,11 +1,23 @@
 package com.example.wordsfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wordsfactory.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding= ActivitySecondBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        binding.backBtn.setOnClickListener {
+            val intent = Intent(this, FirstActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
