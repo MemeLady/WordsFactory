945d3f66b9541d7b5b268851a88c44a64a09971b
MemeLady <alex.kolpakova2001@gmail.com>
2023-03-08 22:38:45 +0700

    Шестой пункт лабораторной работы

--- app/src/main/java/com/example/wordsfactory/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/wordsfactory/ui/notifications/NotificationsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,11 +31,27 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.webView.webViewClient=MyWebViewClient()
+        binding.webView.loadUrl("https://learnenglish.britishcouncil.org/general-english/video-zone")
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+
+class MyWebViewClient:WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        val URL= request?.url.toString()
+        val trueURL="https://learnenglish.britishcouncil.org/general-english/video-zone"
+
+        if(URL.contains(trueURL)){
+            return false
+        }
+        return true
+
     }
 }
