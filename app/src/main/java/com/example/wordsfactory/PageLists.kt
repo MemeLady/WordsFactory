2add28c3ff43a9300a8a5f7f08c4c87ad9d63a24
MemeLady <alex.kolpakova2001@gmail.com>
2023-03-08 21:15:21 +0700

    Словарь

--- app/src/main/java/com/example/wordsfactory/PageLists.kt
+++ app/src/main/java/com/example/wordsfactory/PageLists.kt
@@ -2,8 +2,8 @@ package com.example.wordsfactory
 
 object PageLists {
     val introSlides= listOf(
-        Intro(R.drawable.intro1logo,"Learn anytime\n and anywhere","Quarantine is the perfect time to spend your \n day learning something new, from anywhere!"),
-        Intro(R.drawable.intro2logo,"Find a course \n for you","Quarantine is the perfect time to spend your \n day learning something new, from anywhere!"),
-        Intro(R.drawable.intro3logo,"Improve your skills","Quarantine is the perfect time to spend your \n day learning something new, from anywhere!")
+        Intro(R.drawable.intro1logo,"Learn anytime\nand anywhere","Quarantine is the perfect time to spend your \nday learning something new, from anywhere!"),
+        Intro(R.drawable.intro2logo,"Find a course \nfor you","Quarantine is the perfect time to spend your \nday learning something new, from anywhere!"),
+        Intro(R.drawable.intro3logo,"Improve your skills \n","Quarantine is the perfect time to spend your \nday learning something new, from anywhere!")
     )
 }
